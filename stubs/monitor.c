65d6274233513aba2faf0c86f8b935c7c5ca7cef
Jagannathan Raman <jag.raman@oracle.com>
2019-02-13 11:02:21 -0500

    multi-process: Add stub functions to facilate build of multi-process
    Add stub functions that are needed during compile time but not in
    runtime.
    
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>

--- stubs/monitor.c
+++ stubs/monitor.c
@@ -3,6 +3,8 @@
 #include "qapi/qapi-emit-events.h"
 #include "qemu-common.h"
 #include "monitor/monitor.h"
+#include "qapi/qapi-types-misc.h"
+#include "qapi/qapi-commands-misc.h"
 
 __thread Monitor *cur_mon;
 
@@ -17,5 +19,28 @@ void monitor_init(Chardev *chr, int flags)
 }
 
 void qapi_event_emit(QAPIEvent event, QDict *qdict)
+{
+}
+
+int monitor_get_cpu_index(void)
+{
+    return -ENOSYS;
+}
+void monitor_printf(Monitor *mon, const char *fmt, ...)
+{
+}
+
+bool monitor_cur_is_qmp(void)
+{
+    return false;
+}
+
+ObjectPropertyInfoList *qmp_device_list_properties(const char *typename,
+                                                   Error **errp)
+{
+    return NULL;
+}
+
+void monitor_vfprintf(FILE *stream, const char *fmt, va_list ap)
 {
 }

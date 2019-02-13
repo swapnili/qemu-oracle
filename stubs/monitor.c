29bbfa028904a603b708801897872fa250a6bbaa
Jagannathan Raman <jag.raman@oracle.com>
2019-02-13 11:02:21 -0500

    multi-process: Add stub functions to facilate build of multi-process
    Add stub functions that are needed during compile time but not in
    runtime.
    
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>

--- stubs/monitor.c
+++ stubs/monitor.c
@@ -2,6 +2,12 @@
 #include "qapi/error.h"
 #include "qapi/qapi-emit-events.h"
 #include "monitor/monitor.h"
+#include "qapi/qapi-types-misc.h"
+#include "qapi/qapi-commands-misc.h"
+#include "qapi/qapi-types-qom.h"
+#include "qapi/qapi-commands-qdev.h"
+#include "hw/qdev-core.h"
+#include "sysemu/sysemu.h"
 
 __thread Monitor *cur_mon;
 
@@ -26,4 +32,31 @@ void monitor_init_hmp(Chardev *chr, bool use_readline)
 
 void qapi_event_emit(QAPIEvent event, QDict *qdict)
 {
+}
+
+int monitor_get_cpu_index(void)
+{
+    return -ENOSYS;
+}
+int monitor_printf(Monitor *mon, const char *fmt, ...)
+{
+    return -ENOSYS;
+}
+
+bool monitor_cur_is_qmp(void)
+{
+    return false;
+}
+
+ObjectPropertyInfoList *qmp_device_list_properties(const char *typename,
+                                                   Error **errp)
+{
+    return NULL;
+}
+
+VMChangeStateEntry *qdev_add_vm_change_state_handler(DeviceState *dev,
+                                                     VMChangeStateHandler *cb,
+                                                     void *opaque)
+{
+    return NULL;
 }

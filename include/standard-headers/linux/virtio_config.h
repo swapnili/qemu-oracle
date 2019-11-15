4e27eab71213c3fc681c0ee4488fac0662dd980e
Cornelia Huck <cohuck@redhat.com>
2019-11-15 08:47:38 -0600

    linux headers: update against Linux 5.2-rc1
    commit a188339ca5a396acc588e5851ed7e19f66b0ebd9
    
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    (cherry picked from commit d9cb4336159a00bd0d9c81b93f02874ef3626057)
    
    JIRA: https://jira.oci.oraclecorp.com/browse/LINUX-2232
    
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Karl Heubaum <karl.heubaum@oracle.com>
    Signed-off-by: Mark Kanda <mark.kanda@oracle.com>
    
    Conflicts:
    	include/standard-headers/drivers/infiniband/hw/vmw_pvrdma/pvrdma_dev_api.h
    	include/standard-headers/drm/drm_fourcc.h
    	include/standard-headers/linux/input-event-codes.h
    	include/standard-headers/linux/virtio_gpu.h
    	include/standard-headers/linux/virtio_ring.h
    	linux-headers/asm-arm/unistd-common.h
    	linux-headers/asm-arm64/unistd.h
    	linux-headers/asm-generic/mman-common.h
    	linux-headers/asm-generic/unistd.h
    	linux-headers/asm-mips/mman.h
    	linux-headers/asm-mips/unistd_n32.h
    	linux-headers/asm-mips/unistd_n64.h
    	linux-headers/asm-mips/unistd_o32.h
    	linux-headers/asm-powerpc/unistd_32.h
    	linux-headers/asm-powerpc/unistd_64.h
    	linux-headers/linux/kvm.h
    	linux-headers/linux/mman.h
    	linux-headers/linux/vfio.h
    	include/standard-headers/drm/drm_fourcc.h
    
    Resolved various conflicts due to upstream code churn.

--- include/standard-headers/linux/virtio_config.h
+++ include/standard-headers/linux/virtio_config.h
@@ -76,6 +76,12 @@
 #define VIRTIO_F_IOMMU_PLATFORM		33
 
 /*
+ * This feature indicates that memory accesses by the driver and the
+ * device are ordered in a way described by the platform.
+ */
+#define VIRTIO_F_ORDER_PLATFORM		36
+
+/*
  * Does the device support Single Root I/O Virtualization?
  */
 #define VIRTIO_F_SR_IOV			37

20b97a5278c3aa1facc8710c522521191c0f9ae9
Elena Ufimtseva <elena.ufimtseva@oracle.com>
2020-02-22 10:41:19 -0800

    multi-process: introduce proxy object
    Defines a PCI Device proxy object as a child of TYPE_PCI_DEVICE.
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>

---
+++ include/hw/pci/proxy.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2018, 2020 Oracle and/or its affiliates.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#ifndef PROXY_H
+#define PROXY_H
+
+#include "hw/pci/pci.h"
+#include "io/channel.h"
+
+#define TYPE_PCI_PROXY_DEV "pci-proxy-dev"
+
+#define PCI_PROXY_DEV(obj) \
+            OBJECT_CHECK(PCIProxyDev, (obj), TYPE_PCI_PROXY_DEV)
+
+typedef struct PCIProxyDev {
+    PCIDevice parent_dev;
+    char *fd;
+
+    /* Mutex used to protect the ioc fd from
+     * concurrent access since proxy waits for the
+     * replies from the remote.
+     */
+    QemuMutex io_mutex;
+    QIOChannel *ioc;
+} PCIProxyDev;
+
+#endif /* PROXY_H */
